ee1f863add36333003039a716947965e04e99b0d
finnetrolle <maxsyachin@gmail.com>
2020-01-12 08:46:38 -0500

    file separator fix to be able to run on Linux

--- engine/src/main/java/ru/finnetrolle/scripting/harbinger/runner/ExampleRunner.java
+++ engine/src/main/java/ru/finnetrolle/scripting/harbinger/runner/ExampleRunner.java
@@ -1,24 +1,47 @@
 package ru.finnetrolle.scripting.harbinger.runner;
 
-import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.finnetrolle.scripting.harbinger.Harbinger;
 import ru.finnetrolle.scripting.harbinger.util.Pair;
 
+import java.io.File;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class ExampleRunner {
 
   static final String[] PATH_TO_DIR_WITH_SOURCES = {
-      "processors\\src\\main\\groovy\\ru\\finnetrolle\\scripting\\harbinger\\processors\\"};
-  static final Map<String, String> SOURCE = Stream.of(
-      Pair.from("1", "1000"),
-      Pair.from("2", "season"),
-      Pair.from("3", "world"),
-      Pair.from("4", "123"),
-      Pair.from("5", "322.48")
-  ).collect(Collectors.toMap(Pair::getKey, Pair::getValue));
+    "processors"
+        + File.separator
+        + "src"
+        + File.separator
+        + "main"
+        + File.separator
+        + "groovy"
+        + File.separator
+        + "ru"
+        + File.separator
+        + "finnetrolle"
+        + File.separator
+        + "scripting"
+        + File.separator
+        + "harbinger"
+        + File.separator
+        + "processors"
+        + File.separator
+  };
+
+  static final Map<String, String> SOURCE =
+      Stream.of(
+              Pair.from("1", "1000"),
+              Pair.from("2", "season"),
+              Pair.from("3", "world"),
+              Pair.from("4", "123"),
+              Pair.from("5", "322.48"))
+          .collect(Collectors.toMap(Pair::getKey, Pair::getValue));
+
   private static final Logger LOG = LoggerFactory.getLogger(ExampleRunner.class);
 
   static void logMap(Map<String, String> map) {
@@ -35,6 +58,4 @@ public class ExampleRunner {
     harbinger.loadProcessors("example2.ini");
     logMap(harbinger.process(SOURCE));
   }
-
-
 }
